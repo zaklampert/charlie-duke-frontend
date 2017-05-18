ddebf2db979796726398b8147bcee417c28d623b
Zak Lampert <zaklampert@gmail.com>
2017-05-18 15:30:24 -0500

    no need to console log

--- app/src/data.js
+++ app/src/data.js
@@ -34,8 +34,6 @@ const lookupChildTemplate = (child) => {
 }
 export const mapDataToPage = (dataFromWordpress) => {
   return dataFromWordpress && dataFromWordpress.map((page, i)=>{
-    // console.log(page);
-    console.log(page)
     return {
       template: lookupTemplate(page),
       title: page.title,
