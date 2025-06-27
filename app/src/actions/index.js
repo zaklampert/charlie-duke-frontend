920c386d54df9ae8426e7edc6a189ad8c676ff41
Zak Lampert <zaklampert@gmail.com>
2025-06-27 15:53:29 -0600

    Replace WordPress products with Shopify

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,4 +1,4 @@
-import { mapDataToPage, mapEvents, mapProducts } from '../data';
+import { mapDataToPage, mapEvents, mapShopifyProducts } from '../data';
 import { pauseHowls } from '../helpers';
 import 'isomorphic-fetch';
 
@@ -11,6 +11,7 @@ export const HIDE_MODAL = 'HIDE_MODAL';
 export const UPDATE_LOCATION = 'UPDATE_LOCATION';
 
 const API_URL = 'https://charlieduke.wpengine.com';
+const SHOPIFY_URL = 'https://shop.charlieduke.com';
 const apiOptions = {
   // headers:{
   // 'Access-Control-Allow-Origin':'*',
@@ -22,48 +23,6 @@ const apiOptions = {
   // },
   mode: 'cors',
 }
-
-export const onStripeToken = ({
-    amount,
-    success,
-    error,
-    inscription,
-    product_description
-  }, token, args ) => {
-
-  let myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/json');
-  fetch(`${API_URL}/wp-json/duke/v1/stripe/charge`, {
-     method: 'POST',
-     headers: myHeaders,
-     mode: 'cors',
-     cache: 'default',
-     body: JSON.stringify({
-       token: token.id,
-       amount,
-       inscription,
-       product_description,
-       shipping: {
-         name: args.shipping_name,
-         address: {
-           line1: args.shipping_address_line1,
-           line2: args.shipping_address_line2,
-           postal_code: args.shipping_address_zip,
-           state: args.shipping_address_state,
-           city: args.shipping_address_country_code,
-         }
-       },
-       email: token.email,
-     }),
-   }).then(response => {
-     response.json().then(data => {
-       success(data);
-     });
-   }).catch(e=>{
-     error(e);
-   })
-}
-
 
 const receivePages = (json) => ({
   type: POPULATE_PAGES,
@@ -138,10 +97,10 @@ const getEvents = () => dispatch => {
 }
 
 const getProducts = () => dispatch => {
-  return fetch(`${API_URL}/wp-json/wp/v2/product?per_page=100&_embed`, apiOptions)
+  return fetch(`${SHOPIFY_URL}/products.json`)
     .then(response => response.json())
     .then(products => {
-      dispatch(receiveProducts(mapProducts(products)));
+      dispatch(receiveProducts(mapShopifyProducts(products)));
     });
 };
 
